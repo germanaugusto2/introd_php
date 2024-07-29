9d872437fbbae1ba2bdd5679ba67800337eafd91
germanaugusto2 <germanordonez@colegioguanenta.edu.co>
2024-07-29 18:17:10 -0500

    se entrega ejercicios de php

---
+++ pagina2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <!-- Icono de la página -->
+    <link rel="icon" type="image/png" href="./img/logo.png">
+    <!-- Hojas de estilo de la página -->
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+<h1>Sistemas</h1>
+<h2>¿Cuánto es?</h2>
+<?php
+    $a = 10;
+    $b = 15;
+    echo "La suma de 10 y 15 es: " . ($a + $b) . "<br/>";
+    echo "El residuo de 15 y 10: " . ($b % $a) . "<br/>";
+    echo "La variable \$a con incremento: " . (++$a) . "<br/>";
+?>
+</body>
+</html>
